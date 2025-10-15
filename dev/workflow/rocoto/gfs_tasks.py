9a822a9c747519ea3dca5073a8be4a3187d9c51d
Bo Huang <bo.huang@noaa.gov>
2025-10-15 18:06:11 +0000

    Generate xml files for GSI and JEDI EnKF-only as expected

--- dev/workflow/rocoto/gfs_tasks.py
+++ dev/workflow/rocoto/gfs_tasks.py
@@ -106,7 +106,7 @@ class GFSTasks(Tasks):
 
         deps = []
 
-        if not self.options['doenkfonly_atm']:
+        if not self.options['do_enkfonly_atm']:
             dep_dict = {'type': 'metatask', 'name': 'gdas_atmos_prod', 'offset': f"-{timedelta_to_HMS(self._base['interval_gdas'])}"}
             deps.append(rocoto.add_dependency(dep_dict))
             data = f'{atm_hist_path}/gdas.t@Hz.atmf009.nc'
@@ -115,7 +115,7 @@ class GFSTasks(Tasks):
         data = f'{dump_path}/{self.run}.t@Hz.updated.status.tm00.bufr_d'
         dep_dict = {'type': 'data', 'data': data}
         deps.append(rocoto.add_dependency(dep_dict))
-        if not self.options['doenkfonly_atm']:
+        if not self.options['do_enkfonly_atm']:
             dep_dict = {'type': 'metatask', 'name': 'gdas_fcst', 'offset': f"-{timedelta_to_HMS(self._base['interval_gdas'])}"}
         else:
             dep_dict = {'type': 'metatask', 'name': 'enkfgdas_epmn', 'offset': f"-{timedelta_to_HMS(self._base['interval_gdas'])}"}
@@ -2392,7 +2392,7 @@ class GFSTasks(Tasks):
         dep_next_fcst_seg = rocoto.create_dependency(dep_condition='or', dep=deps)
         deps = []
         if 'enkf' in self.run:
-            if not self.options['doenkfonly_atm']:
+            if not self.options['do_enkfonly_atm']:
                 dep_dict = {'type': 'task', 'name': f'{self.run}_earc_vrfy'}
                 deps.append(rocoto.add_dependency(dep_dict))
             if self.options['do_archcom']:
@@ -2784,7 +2784,7 @@ class GFSTasks(Tasks):
             return grp, dep, lst
 
         deps = []
-        if not self.options['doenkfonly_atm']:
+        if not self.options['do_enkfonly_atm']:
             dep_dict = {'type': 'task', 'name': f'{self.run.replace("enkf", "")}_analcalc'}
             deps.append(rocoto.add_dependency(dep_dict))
         dep_dict = {'type': 'task', 'name': f'{self.run}_eupd'}
@@ -2826,7 +2826,7 @@ class GFSTasks(Tasks):
     def ecen_fv3jedi(self):
 
         deps = []
-        if not self.options['doenkfonly_atm']:
+        if not self.options['do_enkfonly_atm']:
             dep_dict = {'type': 'task', 'name': f"{self.run.replace('enkf', '')}_atmanlfinal"}
             deps.append(rocoto.add_dependency(dep_dict))
         dep_dict = {'type': 'task', 'name': f'{self.run}_atmensanlfinal'}
@@ -2887,7 +2887,7 @@ class GFSTasks(Tasks):
             dep_dict = {'type': 'task', 'name': f'{self.run}_atmensanlfinal'}
             deps.append(rocoto.add_dependency(dep_dict))
         else:
-            if not self.options['doenkfonly_atm']:
+            if not self.options['do_enkfonly_atm']:
                 dep_dict = {'type': 'task', 'name': f'{self.run.replace("enkf", "")}_analcalc'}
                 deps.append(rocoto.add_dependency(dep_dict))
             dep_dict = {'type': 'task', 'name': f'{self.run}_eupd'}
@@ -2918,7 +2918,7 @@ class GFSTasks(Tasks):
 
         deps = []
         if self.options['do_jediatmens']:
-            #if not self.options['doenkfonly_atm']:
+            #if not self.options['do_enkfonly_atm']:
             dep_dict = {'type': 'task', 'name': f'{self.run}_ecen_fv3jedi'}
             deps.append(rocoto.add_dependency(dep_dict))
             #else:
@@ -3068,7 +3068,7 @@ class GFSTasks(Tasks):
         if 'enkfgdas' in self.run:
             dep_dict = {'type': 'metatask', 'name': f'{self.run}_epmn'}
             deps.append(rocoto.add_dependency(dep_dict))
-            if not self.options['doenkfonly_atm']:
+            if not self.options['do_enkfonly_atm']:
                 if not self.options['do_jediatmvar']:
                     dep_dict = {'type': 'task', 'name': f'{self.run}_echgres'}
                     deps.append(rocoto.add_dependency(dep_dict))
@@ -3104,7 +3104,7 @@ class GFSTasks(Tasks):
         if 'enkfgdas' in self.run:
             dep_dict = {'type': 'metatask', 'name': f'{self.run}_epmn'}
             deps.append(rocoto.add_dependency(dep_dict))
-            if not self.options['doenkfonly_atm']:
+            if not self.options['do_enkfonly_atm']:
                 if not self.options['do_jediatmvar']:
                     dep_dict = {'type': 'task', 'name': f'{self.run}_echgres'}
                     deps.append(rocoto.add_dependency(dep_dict))
